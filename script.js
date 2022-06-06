87f1516f6e0440b49e29de848e0656f39d47030e
garciasole <gfsoled@gmail.com>
2022-06-06 17:11:11 +0800

    Added style to game,background and title

--- script.js
+++ script.js
@@ -17,6 +17,6 @@ var checkDead = setInterval(function(){
   if (blockLeft<20 && blockLeft>0 && characterTop>=130){
     block.style.animation = "none";
     block.style.display = "none";
-    alert("u lose :(");
+    alert("GAME OVER" + "\n" + "you fell in love with a fuckboy :(");
   }
 },10);

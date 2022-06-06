77f47514536f423a22c721c62f21cf2ca90207b5
garciasole <gfsoled@gmail.com>
2022-06-06 15:34:31 +0800

    Game over popup created

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function jump(){
       character.classList.remove("animate");  
     },500);
 }
+
+var checkDead = setInterval(function(){
+  var characterTop =
+  parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  var blockLeft =
+  parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+  if (blockLeft<20 && blockLeft>0 && characterTop>=130){
+    block.style.animation = "none";
+    block.style.display = "none";
+    alert("u lose :(");
+  }
+},10);
